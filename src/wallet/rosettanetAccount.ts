40c0898fad1f7ccc9fa3071e4655a7b1b762341c
Bora Atik <bora_atik98@hotmail.com>
2025-05-13 20:21:33 +0300

    chore: changed to address in execute function, removed logs

--- src/wallet/rosettanetAccount.ts
+++ src/wallet/rosettanetAccount.ts
@@ -323,26 +323,18 @@ export class RosettanetAccount extends Account implements AccountInterface {
       };
     });
 
-    console.log('calls', calls);
-
-    console.log('txCalls', txCalls);
-
     const params = {
       calls: txCalls,
     };
-
-    console.log('params', params);
 
     const txData = prepareMulticallCalldata(params.calls);
 
     const txObject = {
       from: this.address,
-      to: this.address,
+      to: '0x0000000000000000000000004645415455524553',
       data: txData,
       value: '0x0',
     };
-
-    console.log('txObject', txObject);
 
     const txHash = await sendTransaction(this.walletProvider, txObject);
     return { transaction_hash: txHash };
