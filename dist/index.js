2102d1c5aa3cd95d2f4d1187cd579d0d689ccc23
Bora Atik <bora_atik98@hotmail.com>
2025-05-13 22:06:51 +0300

    chore: new build files 3

--- dist/index.js
+++ dist/index.js
@@ -38,8 +38,8 @@ var __toESM = (mod, isNodeMode, target) => (
 var __toCommonJS = (mod) => __copyProps(__defProp({}, '__esModule', { value: true }), mod);
 
 // src/index.ts
-var index_exports = {};
-__export(index_exports, {
+var src_exports = {};
+__export(src_exports, {
   Account: () => Account,
   AccountInterface: () => AccountInterface,
   BatchClient: () => BatchClient,
@@ -151,7 +151,7 @@ __export(index_exports, {
   validateChecksumAddress: () => validateChecksumAddress,
   wallet: () => connect_exports,
 });
-module.exports = __toCommonJS(index_exports);
+module.exports = __toCommonJS(src_exports);
 
 // src/global/constants.ts
 var constants_exports = {};
